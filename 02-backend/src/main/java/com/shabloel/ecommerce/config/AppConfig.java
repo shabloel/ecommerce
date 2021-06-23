c46912bae47330181813107f4eba09f796cc4efd
shabloel <shabloel@outlook.com>
2021-06-23 22:29:30 +0200

    config change

--- 02-backend/src/main/java/com/shabloel/ecommerce/config/AppConfig.java
+++ 02-backend/src/main/java/com/shabloel/ecommerce/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.shabloel.ecommerce.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @since :  25-5-2021, di
  **/
 @Configuration
+@EnableWebMvc //for the restcontroller
 public class AppConfig implements WebMvcConfigurer {
 
     @Value("${allowed.origins}")
